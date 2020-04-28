87a7a3446e561850502feb84664ae71724469245
issam <issamwah@yahoo.fr>
2020-04-28 02:38:01 +0200

    Initial commit

---
+++ ajouter.php
@@ -0,0 +1,61 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <!-- GOOGLE FONT -->
+    <link href="assets/css/css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css "/>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></head>
+<body>
+
+
+<form action="" role="form" method="post">
+
+<div class="form-group">
+    
+<div class="label label-primary">
+NOM produit:</div>
+ <input type="text" name="nomproduitfield">
+
+
+
+
+</div>
+<div class="form-group">
+    
+<div class="label label-primary">
+Quantité: </div>
+ <input type="text" name="quantitefield">
+
+
+
+
+</div>
+
+<button type="submit" name="add" class="btn btn-success" >Ajouter</button>
+<button class="btn btn-primary"><a href="search.php"> search</a></button>
+<button type="submit" name="imprimer" class="btn btn-danger" >Imprimer</button>
+
+</body>
+</html>
+
